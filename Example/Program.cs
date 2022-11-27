5644908a4936969d710a3c4181d99de57c756bf2
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2022-11-27 18:09:46 +0100

    Add support to various string types, and diagnostic errors

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,16 +1,24 @@
-﻿using TypeExtensions.Generated;
+﻿using System.Text.Json;
+using TypeExtensions.Generated;
 internal class Program
 {
-    [EmitType] private static string SampleType { get;  } = "{ \"boolVal\" : true, \"numVal\" : 23.69, \"objVal\" : {\"nestedField\" : \"23\"}, \"arrVal\" : [23, 69], \"arrobjVal\" : [{\"nestedField\" : \"23\"}] }";
+    [EmitType] private static string SampleType { get; } = @"{ 
+            ""boolVal"" : true, 
+            ""numVal"" : 23.69, 
+            ""objVal"" : {
+                ""nestedField"" : ""23""
+            }, 
+            ""arrVal"" : [23, 69], 
+            ""arrobjVal"" : [
+                {
+                    ""nestedField"" : ""23""
+                }
+            ] 
+    }";
 
     private static void Main(string[] args)
     {
-        SampleType test = new SampleType();
-        test.boolVal = true;
-        test.numVal = 23.69M;
-        test.objVal = new Objval_T
-        {
-            nestedField = "12"
-        };
+        SampleType test = JsonSerializer.Deserialize<SampleType>(SampleType);
+        Console.WriteLine(test.numVal);
     }
 }
--- TypeProvider/TypeProvider.cs
+++ TypeProvider/TypeProvider.cs
@@ -9,13 +9,33 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Text;
 using System.Text.Json;
+using System.Data;
 
-public static class StringExt {
-        public static string ToPascal(this string Identifier) 
-            =>  CultureInfo.CurrentCulture
-                            .TextInfo
-                            .ToTitleCase(Identifier.ToLower().Replace("_", " ")).Replace(" ", string.Empty);
-} 
+public class InvalidPropertyType : Exception { }
+public class InvalidPropertyAccess : Exception { }
+public class InvalidPropertyValue : Exception { }
+
+public static class ToolExtensions {
+    public static string ToPascal(this string Identifier) 
+        =>  CultureInfo.CurrentCulture
+                        .TextInfo
+                        .ToTitleCase(Identifier.ToLower().Replace("_", " ")).Replace(" ", string.Empty);
+
+    public static DiagnosticDescriptor Rule(Exception ex) => new DiagnosticDescriptor(
+        id: "JTP01",
+        title: "Argument format error",
+        category: "Design",
+        defaultSeverity: DiagnosticSeverity.Error,
+        isEnabledByDefault: true,
+        description: "Property Type Emission must be : Readonly, a string and private static",
+        messageFormat: ex switch
+        {
+            InvalidPropertyType _ => $"Build failed with Error : {nameof(InvalidPropertyType)} (Tagged property must be a string)",
+            InvalidPropertyValue _ => $"Build failed with Error : {nameof(InvalidPropertyValue)} (Tagged property must be a correct json)",
+            InvalidPropertyAccess _ => $"Build failed with Error : {nameof(InvalidPropertyAccess)} (Tagged property must be readonly)",
+        });
+
+}
 public class TypeInstantiator
 {
     string emitForm(IEnumerable<(string type, string name)> props)
@@ -111,17 +131,22 @@ public class TypeInstantiator
     }
     public string EmitType(string name, string sample, bool isRecord = false)
     {
-        var trimmed_Value = sample.Substring(1, sample.Length - 2).Replace("\\", String.Empty);
-        JsonDocument tree = JsonDocument.Parse(trimmed_Value, new JsonDocumentOptions() { AllowTrailingCommas = true });
-        var typename = GetObjectType(tree.RootElement.EnumerateObject(), name);
-        StringBuilder sb = new StringBuilder();
-        foreach (var type in emmited_types)
+        try
         {
-            bool isRoot = type.Value.Item1.Contains("__GeneratedType__");
-            var type_code = TypeTemplate("public" /*isRoot ? "file" : "public"*/, type.Value.Item1, type.Value.Item2, "record");
-            sb.Append(type_code);
+            JsonDocument tree = JsonDocument.Parse(sample, new JsonDocumentOptions() { AllowTrailingCommas = true });
+            var typename = GetObjectType(tree.RootElement.EnumerateObject(), name);
+            StringBuilder sb = new StringBuilder();
+            foreach (var type in emmited_types)
+            {
+                bool isRoot = type.Value.Item1.Contains("__GeneratedType__");
+                var type_code = TypeTemplate("public" /*isRoot ? "file" : "public"*/, type.Value.Item1, type.Value.Item2, "record");
+                sb.Append(type_code);
+            }
+            return FileTemplate(sb.ToString());
+        } catch {
+            throw new InvalidPropertyValue();
         }
-        return FileTemplate(sb.ToString());
+        
     }
 }
 
@@ -137,6 +162,17 @@ public class TypesGenerator : ISourceGenerator
             .SelectMany(propDef =>
             {
                 var semanticModel = context.GetSemanticModel(propDef.SyntaxTree);
+                var type = semanticModel.GetTypeInfo(propDef.Type).Type;
+                if (type.SpecialType != SpecialType.System_String)
+                {
+                    throw new InvalidPropertyType();
+                }
+
+                if (propDef.AccessorList.Accessors.Any(SyntaxKind.SetAccessorDeclaration))
+                {
+                    throw new InvalidPropertyAccess();
+                }
+
                 return propDef.AttributeLists
                     .SelectMany(x => x.Attributes)
                     .Where(attr =>
@@ -146,7 +182,7 @@ public class TypesGenerator : ISourceGenerator
                     }).Select(attr =>
                     {
                         var typename = propDef.Identifier.Value.ToString();
-                        var typesample = propDef.Initializer.Value.ToString();
+                        var typesample = semanticModel.GetConstantValue(propDef.Initializer.Value).ToString();
                         return (typename, typesample);
                     });
             }).ToArray();
@@ -154,14 +190,20 @@ public class TypesGenerator : ISourceGenerator
     public void Execute(GeneratorExecutionContext context)
     {
         var engine = new TypeInstantiator();
-        var allSamples = GetAllMarkedProperties(context.Compilation);
-        var sb = new StringBuilder();
-        foreach(var (name, sample) in allSamples)
+        try
         {
-            sb.Append(engine.EmitType(name, sample));
+            var allSamples = GetAllMarkedProperties(context.Compilation);
+            var sb = new StringBuilder();
+            foreach (var (name, sample) in allSamples)
+            {
+                sb.Append(engine.EmitType(name, sample));
+            }
+            context.AddSource("EmitedTypes.g.cs", SourceText.From(sb.ToString(), Encoding.UTF8));
+        }
+        catch (Exception ex){
+            context.ReportDiagnostic(Diagnostic.Create(ToolExtensions.Rule(ex), Location.None));
         }
 
-        context.AddSource("EmitedTypes.g.cs", SourceText.From(sb.ToString(), Encoding.UTF8));
     }
 
     public void Initialize(GeneratorInitializationContext context)
